1041aadd0a062e3dd61df070bff4245e1a00b720
Stephanie Lee <stephaniemjlee@gmail.com>
2021-07-22 01:05:02 +1000

    updated home pages

---
+++ src/components/Typewriter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TypeWriterEffect from 'react-typewriter-effect';
+
+<TypeWriterEffect
+            textStyle={{ fontFamily: 'Red Hat Display' }}
+            startDelay={100}
+            cursorColor="black"
+            text="Full Stack Developer"
+            typeSpeed={100}
+/>
+
+export default Type;
