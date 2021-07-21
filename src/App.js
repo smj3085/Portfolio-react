ad280f015afc59473366a7f2621dc5a4d0c26709
Stephanie Lee <stephaniemjlee@gmail.com>
2021-07-22 01:05:21 +1000

    updated dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import Preloader from "../src/components/Pre";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+// import About from "./components/About";
+// import Projects from "./components/Projects";
+// import Contact from "./components/Contact";
+// import Footer from "./components/Footer";
+// import Resume from "./components/Resume";
+// import "./style.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import ScrollToTop from "./components/ScrollToTop";
+
+class App extends Component {
+  render() {
+    return (      
+       <BrowserRouter>
+        <div>
+          <Navbar />
+            <Switch>
+             <Route path="/" component={Home} exact/>
+             {/* <Route path="/about" component={About}/>
+             <Route path="/contact" component={Contact}/>
+             <Route path="/projects" component={Projects}/>
+             <Route path="/resume" component={Resume}/> */}
+           </Switch>
+          {/* <Footer /> */}
+        </div> 
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App;
+  
+  export default App;
