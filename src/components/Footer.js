df985f88ba92d70b5ea9a11bfe75ee3edf99f310
Stephanie Lee <stephaniemjlee@gmail.com>
2021-07-23 00:40:01 +1000

    updated footer page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,43 +1,16 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { FaLinkedinIn, FaGithubSquare } from "react-icons/fa";
 
 function Footer() {
     return (
       <Container fluid className="footer">
         <Row>
-          <Col md="4" className="footer">
-            <h3>Stephanie Lee 2021</h3>
-          </Col>
-          <Col md="4" className="footer-body">
-            <ul className="footer-icons">
-                <li className="social-icons">
-                <a
-                  href="https://github.com/smj3085"
-                  style={{ color: "black" }}
-                  target="_blank" 
-                  rel="noopener noreferrer"
-                >
-                  <FaGithubSquare />
-                </a>
-              </li>
-              
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/stephanielee/"
-                  style={{ color: "black" }}
-                  target="_blank" 
-                  rel="noopener noreferrer"
-                >
-                  <FaLinkedinIn />
-                </a>
-            </li>
-            
-            </ul>
+          <Col md="4" className="footer-name">
+            <h4>Developed by Stephanie Lee</h4>
           </Col>
         </Row>
       </Container>
     );
-  }
+}
   
-  export default Footer;
+export default Footer;
