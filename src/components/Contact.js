93d0e55bf58c6a01701fb4ff2e1c0474843e4cdd
Stephanie Lee <stephaniemjlee@gmail.com>
2021-07-24 14:24:08 +1000

    added form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,10 @@
-// Contact me! 
-
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-// import Button from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button";
 import {FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa"; 
+import Form from 'react-bootstrap/Form'
+
+
 
 function Contact() {
     return (
@@ -12,39 +13,59 @@ function Contact() {
                 <Row className="contact-section" style={{ justifyContent: "center", paddingBottom: "50px" }}>
                 <h1 className="contact-header"><strong className="pink"> Connect</strong> with me!</h1>
             
+            <Col>
+            <br />
 {/* Form */}
+            <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Your name</Form.Label>
+                <Form.Control as="textarea" placeholder="Name" rows={1} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="name@example.com" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea2">
+                <Form.Label>Comment</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+            <Button variant="flat" style={{ background: "#f15bb5", color: "white", margin: 10}}>
+                Submit
+            </Button>
+            </Form>
+            </Col>
 
-                <Col xs={4} md={3} className="social-links">
-                    <a
+            <Col>
+            <div xs={4} md={3} className="social-links">
+                <a
                     href="https://github.com/3085"
                     target="_blank"
                     rel="noreferrer"
-                    className="icon-colour  social-icons"
+                    className="social-icons" style={{color: "#3a86ff"}}
                     >
                     <FaGithub />
                     </a>
-                </Col>
-                <Col xs={4} md={3} className="social-links">
+  
                     <a
                     href="stephaniemjlee@gmail.com"
                     target="_blank"
                     rel="noreferrer"
-                    className="icon-colour  social-icons"
+                    className="social-icons" style={{color: "#3a86ff"}}
                     >
                     <FaEnvelope />
                     </a>
-                </Col>
-                <Col xs={4} md={3} className="social-links">
+
                 <a
                     href="https://www.linkedin.com/in/stephanie-lee-syd/"
                     target="_blank"
                     rel="noreferrer"
-                    className="icon-colour  social-icons"
+                    className="social-icons" style={{color: "#3a86ff"}}
                     >
                     <FaLinkedin />
                     </a>
+                </div>
                 </Col>
-
+     
             </Row>
 
             </Container>
